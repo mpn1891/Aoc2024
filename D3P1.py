32da6d855f18a61a67ba4d76d5b9607ad44ff45f
Matt Martin <mattmartin1891@gmail.com>
2024-12-03 17:21:17 -0500

    Day 3 completed

---
+++ D3P1.py
@@ -0,0 +1,49 @@
+
+def main():
+    array = []
+
+    p1 = 0
+    p2 = 0
+    # Input processing
+    with open(r"C:\Users\mpn42\PycharmProjects\AoC\AoC2024\Inputs\D3.txt", "r") as f:
+        for line in f.read().splitlines():
+            array.append(line.split('mul'))
+
+    print(array)
+
+
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+
+            wrk = []
+            if array[i][j].find('(') == 0 and array[i][j].find(',') != -1 and array[i][j].find(')') != -1:
+                wrk.clear()
+
+                wrk = array[i][j].split(',')
+
+                wrk[0] = wrk[0][wrk[0].find('(' ) +1:]
+
+                wrk[1] = wrk[1][:wrk[1].find(')')]
+
+                if wrk[0].isnumeric() and wrk[1].isnumeric():
+                    print('IS valid: ', array[i][j])
+
+                    p1 += int(wrk[0]) * int(wrk[1])
+                else:
+                    print('NOT valid: ', array[i][j])
+            else:
+                print('NOT valid: ', array[i][j])
+    print(p1)
+
+
+
+
+
+
+
+    print(p1)
+    print(p2)
+
+
+if __name__ == "__main__":
+    main()
---
+++ D3P2.py
@@ -0,0 +1,38 @@
+import time
+
+
+def main():
+    array = []
+
+    p2 = 0
+    # Input processing
+    with open(r"C:\Users\mpn42\PycharmProjects\AoC\AoC2024\Inputs\D3.txt", "r") as f:
+        for line in f.read().splitlines():
+            array.append(line.split('mul'))
+
+    enabled = True
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+
+            if array[i][j].find('(') == 0 and array[i][j].find(',') != -1 and array[i][j].find(')') != -1 and enabled:
+
+                wrk = array[i][j].split(',')
+                wrk[0] = wrk[0][wrk[0].find('(') + 1:]
+                wrk[1] = wrk[1][:wrk[1].find(')')]
+
+                if wrk[0].isnumeric() and wrk[1].isnumeric():
+                    p2 += int(wrk[0]) * int(wrk[1])
+
+            if array[i][j].find('''don't()''') != -1:
+                enabled = False
+            if array[i][j].find('''do()''') != -1:
+                enabled = True
+    print(p2)
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time} seconds")
